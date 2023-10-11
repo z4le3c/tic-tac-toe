8d2aecb1753688c9f4a43a31c71e830b890af411
zalec <142818531+z4le3c@users.noreply.github.com>
2023-10-11 15:25:31 -0600

    checks for winner

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (function () {
-    const gameBoard = [['','',''],['','',''],['','','']];
+    const gameBoard = [['', '', ''], ['', '', ''], ['', '', '']];
     /*
     *  @param {string} symbol
     *  @param {number} col
@@ -31,16 +31,41 @@ const gameBoard = (function () {
     }
 
     /*
-    *  @return {string}
+    *  @param {string} symbol
+    *  @return {void}
     */
-    const checkForWinner = () => {
-        
+    const checkForWinner = (symbol) => {
+        let rows = new Map();
+        let columns = new Map();
+        let diagonals = new Map();
+        for (let i = 0; i < gameBoard.length; i++) {
+            for (let j = 0; j < gameBoard.length; j++) {
+                let s = gameBoard[i][j]
+                if (s == symbol) {
+                    rows.set(i, (rows.get(i) || 0) + 1);
+                    columns.set(j, (columns.get(j) || 0) + 1);
+                    if (i === j) {
+                        diagonals.set('rl', (diagonals.get('rl') || 0) + 1);
+                        if (i + j === gameBoard.length-1) {
+                            diagonals.set('lr', (diagonals.get('lr') || 0) + 1)
+                        }
+                    } else if (i + j === gameBoard.length-1) {
+                        diagonals.set('lr', (diagonals.get('lr') || 0) + 1)
+                    }
+                    if (rows.get(i) == 3 ||
+                        columns.get(j) == 3 ||
+                        diagonals.get('rl') == 3 ||
+                        diagonals.get('lr') == 3) return true;
+                }
+            }
+        }
+        return false;
     }
 
-    return {setCell, getCell, checkForWinner};
+    return { setCell, getCell, checkForWinner };
 })();
 
-function createPlayer (symbol) {
+function createPlayer(symbol) {
     let score = 0;
     const playerSymbol = symbol;
 
@@ -48,7 +73,7 @@ function createPlayer (symbol) {
     const resetScore = () => score = 0;
     const getPlayerSymbol = () => playerSymbol;
 
-    return {addPoint, resetScore, getPlayerSymbol}
+    return { addPoint, resetScore, getPlayerSymbol }
 }
 
 const gameController = (function () {
@@ -58,6 +83,7 @@ const gameController = (function () {
     const player1 = createPlayer('X');
     const player2 = createPlayer('O');
     let currRound = 0;
+    let gameOn = true;
     let boardInit = false // tells if the board was instanciated before
 
     const buildBoard = () => {
@@ -75,28 +101,35 @@ const gameController = (function () {
             col++;
             if (col == 3) {
                 row++;
-                col = col%3;
+                col = col % 3;
             }
         }
         boardInit = true;
     }
 
     const playRound = (col, row) => {
+        if (!gameOn) return
         console.log(row, col)
-        if (gameBoard.getCell(col, row) != '') 
+        if (gameBoard.getCell(col, row) != '')
             return;
+        let pSymbol;
         if (currRound % 2 == 0) {
-            gameBoard.setCell(player1.getPlayerSymbol(), col, row);
+            pSymbol = player1.getPlayerSymbol()
             turnMessage.textContent = 'Player O Turn';
         } else {
-            gameBoard.setCell(player2.getPlayerSymbol(), col, row);
+            pSymbol = player2.getPlayerSymbol()
             turnMessage.textContent = 'Player X Turn';
         }
+        gameBoard.setCell(pSymbol, col, row);
         currRound++;
         buildBoard()
+        if (currRound > 4 && gameBoard.checkForWinner(pSymbol)) {
+            turnMessage.textContent = `Player ${pSymbol} wins`;
+            gameOn = false;
+        }
     }
 
-    return {buildBoard}
+    return { buildBoard }
 })();
 
 gameController.buildBoard()
